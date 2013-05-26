0f250f596d7a0b8590bd6ac04545b30f18c71df9
Adam Curtis <adam@hoowla.com>
2013-05-26 22:19:18 +0100

    fixed hostname for banner

--- getfrontimage.php
+++ getfrontimage.php
@@ -5,7 +5,10 @@ if (isset($_GET['w'])){
 	$width = "1349";
 }
 //get the host name
-$hostname = "http://www.hoowla.com/";
+$hostname = "http://" . $_SERVER['SERVER_NAME'];
+if ($hostname == "http://localhost"){
+	$hostname .= "/hoowla/frontpage";
+}
 $imageurl = $hostname . "/images/hoowlabanner.png";
 echo "<img style=\"float:left;\" src=\"timthumb.php?src=" .$imageurl ."&w=".$width."&h=460&a=t\" alt=\"\">";
 ?>
