f5fe0e74bb7b60cb6d47f93ff5b4e942f1b1da02
Adam Curtis <adam@hoowla.com>
2013-06-11 14:41:44 +0100

    go cardless

--- processsignup.php
+++ processsignup.php
@@ -1,6 +1,6 @@
 <?php
 // Include the library
-include_once 'gocardlessphp/lib/GoCardless.php';
+include_once 'lib/GoCardless.php';
 
 // Use the sandbox environment for testing
 GoCardless::$environment = 'sandbox';
