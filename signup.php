d088de48743338af3b764424d1cf49dae8dbf465
Adam Curtis <adam@hoowla.com>
2013-06-11 15:02:10 +0100

    new copy

--- signup.php
+++ signup.php
@@ -4,16 +4,7 @@
 </head>
 <?php include('htmlheader.php');?>
 <body>
-
-<div id="wrapper">
-<?php 
-$bannerimgurl = "images/bannerbg.png";
-$bannercontent = "<h1>Referral Scheme</h1><p style=\"padding-top:0px;color:#7b9d3e;font-size: 50px;\">&pound;45</p><p>for evey user you signup</p>";
-include('header.php'); ?>
-<section id="main">
-<div class="whiteblock clearfix">
-<div style="width:80%;margin-left:10%;margin-right:10%;text-align: center;">
-<?php 
+<?php
 include_once 'lib/GoCardless.php';
 
 // Use the sandbox environment for testing
@@ -42,9 +33,19 @@ $subscription_details = array(
 
 // Generate the url
 $subscription_url = GoCardless::new_subscription_url($subscription_details);
+?>
+<div id="wrapper">
+<?php 
+$bannerimgurl = "images/bannerbg.png";
+$bannercontent = "<h1>Sign up now</h1><p style=\"padding-top:0px;color:#7b9d3e;font-size: 50px;\"><a href=\"".$subscription_url."\">Register to pay securly via GoCardless</a></p><p>No payment will be taken until August the 31st and we offer a 100% money back guarantee for the first 6 months.</p>";
+include('header.php'); ?>
+<section id="main">
+<div class="whiteblock clearfix">
+<div style="width:80%;margin-left:10%;margin-right:10%;text-align: center;">
+<?php 
 
 // Display the link
-echo '<a href="'.$subscription_url.'">Sign up now</a>';
+//echo '<a href="'.$subscription_url.'">Sign up now</a>';
 
 ?>
 </div>
