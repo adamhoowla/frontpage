6e95ab601fb70ee18b0a44b299145d97ee19ae05
Edd Turtle <eddturtle@gmail.com>
2013-06-18 15:02:43 +0100

    change referalscheme.php to referralscheme.php

---
+++ referralscheme.php
@@ -0,0 +1,67 @@
+<?php include("header.php"); ?>
+			
+			<section class="dark referral house center">
+				<div class="grid">
+					<div class="col-30">
+						<p>Sign up a solicitor<br />that has 5 users<br />and earn</p>
+						<strong>£225</strong>
+					</div>
+					<div class="col-40">
+						<h1>Referral Scheme</h1>
+						<p>Our competitive offers</p>
+					</div>
+					<div class="col-30">
+						<strong>£45</strong>
+						<p>for each user sign up<br />that convert to<br /> be a paying<br />user of Hoowla</p>
+					</div>
+				</div>
+			</section>
+
+			<section class="plan-includes">
+				<div class="grid">
+					<div class="col-25">
+						<h2>all our plans <span>INCLUDE</span></h2>
+					</div>
+					<div class="col-75">
+						<ul>
+							<li>Unlimited customer support</li>
+							<li>No setup fee</li>
+							<li>Access anywhere</li>
+							<li>Cancel anytime online</li>
+							<li>No contract</li>
+							<li>Automatic backup</li>
+						</ul>
+					</div>
+				</div>
+			</section>
+
+			
+			<section class="light icon-grid">
+
+				<div class="grid">
+					<!-- TODO Many Alts! -->
+					<div class="col-25">
+						<img src="images/phone.png" alt="" />
+						<h3>Reduce Phone Calls</h3>
+						<p>Reduce the hassle with less phone calls and more online bidding.</p>
+					</div>
+					<div class="col-25">
+						<img src="images/speachbubble.png" alt="" />
+						<h3>Improved Communication</h3>
+						<p>Communication is more fluid and accessible with data backed up online.</p>
+					</div>
+					<div class="col-25">
+						<img src="images/arrowbox.png" alt="" />
+						<h3>No Installation Required</h3>
+						<p>All you need is an internet connection to be able to access your account online.</p>
+					</div>
+					<div class="col-25">
+						<img src="images/computericon.png" alt="" />
+						<h3>No Setup Fee</h3>
+						<p>We won’t charge you for the set up, and our software is simple and easy to use.</p>
+					</div>
+				</div>
+
+			</section>
+
+<?php include("footer.php"); ?>
