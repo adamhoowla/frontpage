6d2c3a06b75a7922eb973274da0b742af2474ce6
Adam Curtis <adam@hoowla.com>
2013-06-21 10:56:58 +0100

    spelling update

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 					<div class="col-25">
 						<img src="images/computericon.png" alt="" />
 						<h3>No Setup Fee</h3>
-						<p>We won’t charge you for the set up, and our software is simple and easy to use.</p>
+						<p>We won’t charge you for the set up, our software is simple and easy to use.</p>
 					</div>
 				</div>
 
@@ -44,7 +44,7 @@
 					</div>
 					<div class="col-55 even">
 						<h2>Partner overview of all staff &amp; conveyancing cases</h2>
-						<p>Hoowla gives you a easy to understand snap shot view of your businesses. Data about your firm is displayed to allow you to understand what is and isn't working.</p>
+						<p>Hoowla gives you an easy to understand snap shot view of your business. Data about your firm is displayed to allow you to understand what is and isn't working.</p>
 						<p>No longer do you have to wait for monthly reviews to identify opportunities for better use of your resources.</p>
 						<p>Complete user control allows you to make sure the right people see the right data.</p>
 						<p>Accessed from the cloud the data can be viewed on any mobile, laptop or desktop. Data is automatically backed up at no extra cost or hassle to yourself or your business.</p>
@@ -55,7 +55,7 @@
 					<div class="col-55 odd">
 						<h2>See how each solicitor is performing for the firm in an instant</h2>
 						<p>With multiple solicitors working in a number of branches it can be hard to keep on top of who is performing.</p>
-						<p>Hoowla allows you to monitor statistics per solicitor in your firm and give clients and estate agents to leave feedback for you to read.</p>
+						<p>Hoowla allows you to monitor statistics per solicitor in your firm and give clients and estate agents the ability to leave feedback for you to read.</p>
 					</div>
 					<div class="col-45">
 						<img src="images/socOverview2.jpg" alt="" />
@@ -68,7 +68,7 @@
 					</div>
 					<div class="col-55 even">
 						<h2>Break down of offers and cases for Estate Agents</h2>
-						<p>Estate agents can login to Hoowla too and manage their clients offers. Directly liking it to the clients solicitor.</p>
+						<p>Estate agents can login to Hoowla too and manage their clients' offers. Directly linking it to the client's solicitor.</p>
 						<p>You can now offer your clients a real time view of the sale process. Reducing the number of chase up calls you have to handle.</p>
 					</div>
 				</div>
@@ -76,7 +76,7 @@
 				<div class="grid">
 					<div class="col-55 odd">
 						<h3>Reduce paper work with E-signatures</h3>
-						<p>Hoowla aims to reduce paper work were possible and bring the conveyancing process online.</p>
+						<p>Hoowla aims to reduce paper work where possible and bring the conveyancing process online.</p>
 					</div>
 					<div class="col-45">
 						<img src="images/E-SignatureExample.jpg" alt="" />
