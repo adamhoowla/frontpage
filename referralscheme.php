7069ef0f56ac4447eb4859242642bfd21b5ac797
Edd Turtle <eddturtle@gmail.com>
2013-06-24 11:04:24 +0100

    Update Referral Sch. (Add Form)

--- referralscheme.php
+++ referralscheme.php
@@ -1,5 +1,5 @@
 <?php include("header.php"); ?>
-			
+			<!--
 			<section class="dark referral house center">
 				<div class="grid">
 					<div class="col-33">
@@ -16,6 +16,53 @@
 					</div>
 				</div>
 			</section>
+-->
+
+			<section class="dark referral house center">
+				<div class="grid">
+					<div class="col-50">
+						<h1>Referral Scheme</h1>
+						<p>You can <strong>earn money</strong> by referring Solicitors to sign up to Hoowla.</p>
+						<p>To find out more information about our exclusive offer sign up <strong>today</strong>.</p>
+					</div>
+					<div class="col-50">
+						<!--
+						<form>
+							<label for="email">Email</label>
+							<input type="email" name="email" id="email" value="" />
+							<input type="submit" />
+						</form>
+					-->
+<!-- Begin MailChimp Signup Form -->
+<div id="mc_embed_signup">
+<form action="http://hoowla.us7.list-manage.com/subscribe/post?u=b23342bbf8316f324e616dbb8&amp;id=ca4cada147" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+<div class="mc-field-group">
+	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+</label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+<div class="mc-field-group">
+	<label for="mce-NAME">Name  <span class="asterisk">*</span>
+</label>
+	<input type="text" value="" name="NAME" class="required" id="mce-NAME">
+</div>
+<div class="mc-field-group size1of2">
+	<label for="mce-PHONE">Phone </label>
+	<input type="text" name="PHONE" class="" value="" id="mce-PHONE">
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>	<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+					</div>
+				</div>
+			</section>
+
 
 			<section class="plan-includes">
 				<div class="grid">
