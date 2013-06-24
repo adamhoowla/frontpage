fda2ac9d7d168909ff5c219755c541ca90b7a68e
Edd Turtle <eddturtle@gmail.com>
2013-06-24 11:04:43 +0100

    Update Footer

--- footer.php
+++ footer.php
@@ -31,55 +31,85 @@
 
 		<footer role="contentinfo">
 			<div class="grid">
-				<a href="http://www.clockworkbear.com" class="clockworkbear-logo"><img src="images/clockworkbearLogo.png" alt="web design by Clockwork Bear" /></a>
-				<h4>Contact Us</h4>
-				<p>
-					220 High Street<br />
-					2nd Floor, IndyCube<br />
-					Swansea <br />
-					SA1 1NW<br />
-					01792 515667
-				</p>
-				<p>
-					<a href="mailto:hello@hoowla.com">hello@hoowla.com</a>
-				</p>
+				<div class="col-25">
+					<h2>Address:</h2>
+					<p>
+						220 High Street<br />
+						2nd Floor, IndyCube<br />
+						Swansea <br />
+						SA1 1NW
+					</p>
+					<p>
+						Registered in Wales<br />
+						Company No: 08541543
+					</p>
+				</div>
+
+				<div class="col-25">
+					<h2>Contact:</h2>
+					<p>
+						T: 01792 515667<br />
+						E: <a href="mailto:hello@hoowla.com">hello@hoowla.com</a>
+					</p>
+				</div>
+
+				<div class="col-25">
+					<h2>Find Us:</h2>
+					<ul>
+						<li><a href="https://twitter.com/hoowla" class="icon-twitter"></a></li>
+					</ul>
+					<a href="https://twitter.com/hoowla" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @hoowla</a>
+				</div>
+
+				<div class="col-25">
+					<h2>Design:</h2>
+					<a href="http://www.clockworkbear.com" class="clockworkbear-logo"><img src="images/clockworkbearLogo.png" alt="web design by Clockwork Bear" /></a>
+					<p><a href="http://www.clockworkbear.com">clockworkbear.com</a></p>
+				</div>
+
 			</div>
 		</footer>
 
+
+		<!-- Load jQuery from Google CDN (with fallback) -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 		<script>window.jQuery || document.write('<script src="js/libs/jquery-1.10.1.min.js"><\/script>')</script>
+
+		<!-- Hand-written Hoowla JS -->
 		<script src="js/jhoowla.js"></script>
+
+		<!-- Twitter API Integration -->
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 		
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		<!-- Google Analytics -->
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		  ga('create', 'UA-41852408-1', 'hoowla.com');
+		  ga('send', 'pageview');
+		</script>
 
-  ga('create', 'UA-41852408-1', 'hoowla.com');
-  ga('send', 'pageview');
-
-</script>
-
-<!-- Start of StatCounter Code for Default Guide -->
-<script type="text/javascript">
-//<![CDATA[
-var sc_project=9018282; 
-var sc_invisible=1; 
-var sc_security="715410ac"; 
-var scJsHost = (("https:" == document.location.protocol) ?
-"https://secure." : "http://www.");
-document.write("<sc"+"ript type='text/javascript' src='" +
-scJsHost+
-"statcounter.com/counter/counter_xhtml.js'></"+"script>");
-//]]>
-</script>
-<noscript><div class="statcounter"><a title="free hit
-counter" href="http://statcounter.com/"
-class="statcounter"><img class="statcounter"
-src="http://c.statcounter.com/9018282/0/715410ac/1/"
-alt="free hit counter" /></a></div></noscript>
-<!-- End of StatCounter Code for Default Guide -->
+		<!-- Start of StatCounter Code for Default Guide -->
+		<script type="text/javascript">
+		//<![CDATA[
+		var sc_project=9018282; 
+		var sc_invisible=1; 
+		var sc_security="715410ac"; 
+		var scJsHost = (("https:" == document.location.protocol) ?
+		"https://secure." : "http://www.");
+		document.write("<sc"+"ript type='text/javascript' src='" +
+		scJsHost+
+		"statcounter.com/counter/counter_xhtml.js'></"+"script>");
+		//]]>
+		</script>
+		<noscript><div class="statcounter"><a title="free hit
+		counter" href="http://statcounter.com/"
+		class="statcounter"><img class="statcounter"
+		src="http://c.statcounter.com/9018282/0/715410ac/1/"
+		alt="free hit counter" /></a></div></noscript>
+		<!-- End of StatCounter Code for Default Guide -->
 
 	</body>
 </html>
