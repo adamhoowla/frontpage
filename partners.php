2173d0df53859daac367638af92aeb4fecc89dec
Adam Curtis <adam@hoowla.com>
2013-07-23 15:22:57 +0100

    partners update

--- partners.php
+++ partners.php
@@ -15,10 +15,43 @@
 						<img src="images/peterlynnLogo.png" alt="Peter Lynn">
 					</div>
 					<div class="col-75">
-						<p>This is how they work with us</p>
+<p>Peter Lynn and Partners was established in 1999 to offer top quality commercial and civil advice to businesses and people in Swansea. Since then it has expanded, opening offices in Swansea City Centre, Pontardawe, Penarth as well as Morriston.
+PL&amp;P have 34 members of staff which contain eight partners, nineteen qualified solicitors and paralegals who are backed by a team of seven support staff. They are committed to securing the best outcome for all our clients.</p>
 					</div>
 				</div>
 			</section>
+			<section class="light plan-includes">
+				<div class="grid">
+					<div class="col-25">
+						<img src="images/saracensLogo.png" alt="Saracens Solicitors">
+					</div>
+					<div class="col-75">
+<p>Saracens is a dynamic law firm utilising the latest technological tools coupled with traditional legal principles in order to provide its clients with a seamless and efficient professional service.</p>
+					</div>
+				</div>
+			</section>
+			<section class="light plan-includes">
+				<div class="grid">
+					<div class="col-25">
+						<img src="images/ritsonlawLogo.png" alt="Ritson Law">
+					</div>
+					<div class="col-75">
+<p>ritsonlaw solicitors is the culmination of Jonathan's many years of experience in practice both as a manager and solicitor together with an acute understanding of what legal customers really want from their advisors.</p>
+					</div>
+				</div>
+			</section>
+			<section class="light plan-includes">
+				<div class="grid">
+					<div class="col-25">
+						<img src="images/templeheelisLogo.png" alt="Temple Heelis">
+					</div>
+					<div class="col-75">
+<p>Temple Heelis LLP are specialist Cumbria solicitors with offices in both Kendal and Windermere, offering transparent and cost-effective legal services to private clients and employment and litigation services to business clients. You will find all of our legal services listed within our site including links to our financial services company.</p>
+					</div>
+				</div>
+			</section>
+
+
 
 
 <?php include("footer.php"); ?>
