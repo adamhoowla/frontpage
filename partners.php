d7d53220c7b153c5335f309a0f9d29f473118ee1
Adam Curtis <adam@hoowla.com>
2013-07-23 15:14:03 +0100

    partners page first version

---
+++ partners.php
@@ -0,0 +1,24 @@
+<?php include("header.php"); ?>
+			
+			<section class="dark pricing-banner house center">
+				<div class="grid">
+					<h1>Our Partners</h1>
+					<p>A list of all our partners that support Hoowla.</p>
+					<br />
+					<img src="images/houseicon.png" alt="" />
+				</div>
+			</section>
+
+			<section class="light plan-includes">
+				<div class="grid">
+					<div class="col-25">
+						<img src="images/peterlynnLogo.png" alt="Peter Lynn">
+					</div>
+					<div class="col-75">
+						<p>This is how they work with us</p>
+					</div>
+				</div>
+			</section>
+
+
+<?php include("footer.php"); ?>
